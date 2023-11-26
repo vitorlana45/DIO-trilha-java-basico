e80c988e9284bc8fe97254334465808f1b6e335d
vitor lana <vitoresmerio00@gmail.com>
2023-11-25 22:21:00 -0300

    Programa Finalizado

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,5 +1,27 @@
+import java.util.Locale;
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in).useLocale(Locale.US);
+        StringBuilder sb = new StringBuilder();
+
+       
+        System.out.print("Digite o numero da conta: ");
+        int numeroConta = sc.nextInt(); 
+        System.out.print("Digite a Agencia: ");
+        String agencia = sc.next();
+        sc.nextLine();
+        System.out.print("Digite seu nome: ");
+        String nomeCliente = sc.nextLine();
+        Double saldo = 237.48;
+       
+        System.out.println(); 
+        sb.append("Olá " + nomeCliente + ", Obrigado por criar uma conta em nosso nosso Banco. \n");
+        sb.append("sua AGENCIA: " + agencia + " Conta: " + numeroConta + " e seu SALDO: " + saldo + " já está disponivel para saque.");
+      
+       System.out.println(sb);
+
+       sc.close();
     }
 }
